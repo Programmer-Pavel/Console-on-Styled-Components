8685206ff206ff807fbb6d210136ab6592aa0f86
pavel <pavelkukuskmn388@gmail.com>
2021-03-03 22:46:17 +0300

    added styled-components, write console on styled-components

---
+++ src/components/Console.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+import Flex from "./Flex";
+import Line from "./Line";
+
+const StyledConsole = styled.textarea`
+  width: 100%;
+  height: 70vh;
+  background: black;
+  font-size: 24px;
+  border: none;
+  resize: none;
+  color: ${({color}) => color || 'white'};
+
+  &:focus {
+    outline: none;
+  }
+`
+
+const Console = ({color, ...props}) => {
+    const [lines, setLines] = useState(['C/users/My>'])
+
+    return <Flex>
+        <Flex direction="column" margin="0 10px">
+            {lines.map(line =>
+                <Line color={color}>{line}</Line>
+            )}
+        </Flex>
+        <StyledConsole color={color} {...props}/>
+    </Flex>
+};
+
+export default Console;
